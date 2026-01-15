a8ae2af0982e16e4979899be6fe894aef62e9c01
Ettore Napoli <ettore.napoli@rc.fhsc.net@wmalaspina.rc.fhsc.net>
2026-01-15 14:49:12 +0100

    Added SPMA_iclabel function

--- functions/1_preprocessing/SPMA_iclabel.m
+++ functions/1_preprocessing/SPMA_iclabel.m
@@ -9,7 +9,7 @@ function [EEG] = SPMA_iclabel(EEG, opt)
 % Parameters:
 %       EEG (struct): EEG struct using EEGLAB struct system. ICs weights must
 %       have been computed
-% 
+%
 % Other parameters:
 %       Version (string): 'default', 'lite', 'beta'
 %       Save (logical): whether to save or not the dataset with labeled
@@ -18,3 +18,54 @@ function [EEG] = SPMA_iclabel(EEG, opt)
 % See also: EEGLAB, pop_iclabel
 %
 % Authors: Ettore Napoli, University of Bologna, 2026
+
+    arguments (Input)
+        EEG struct
+        % Optional
+        opt.Version string {mustBeMember(opt.Version, ["default", "lite", "beta"])}
+        % Save Options
+        opt.Save logical
+        opt.SaveName string
+        opt.OutputFolder string
+        % Log Options
+        opt.LogEnabled logical
+        opt.LogLevel double {mustBeInteger, mustBeInRange(opt.LogLevel, 0,6)}
+        opt.LogToFile logical
+        opt.LogFileDir string
+        opt.LogFileName string
+    end
+    
+    %% Constants
+    module = "preprocessing";
+    
+    %% Parsing arguments
+    config = SPMA_loadConfig(module, "iclabel", opt);
+    
+    %% Logger
+    logConfig = SPMA_loadConfig(module, "logging", opt);
+    log = SPMA_loggerSetUp(module, logConfig);
+    
+    %% Check prerequisites
+    if isempty(EEG.icaweights)
+        error('SPMA: No ICA', 'No ICs weights found for the current EEG structure.')
+    end
+    
+    %% Run ICLabel
+    log.info(sprintf("Starting ICLabel classification with versino %s", config.Version));
+    
+    try
+        EEG = pop_iclabel(EEG, char(config.Version));
+    
+    catch ME
+        % If something goes wrong, log it and rethrow the error
+        log.error(sprintf("Error during IClabel classification: %s", ME.message));
+    end
+    
+    %% Save
+    if config.Save
+        logParams = unpackStruct(logConfig);
+        SPMA_saveData(EEG, "Name", config.SaveName, "Folder", module,"OutputFolder", config.OutputFolder, logParams{:});
+    end
+end
+
+
